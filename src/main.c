8e62fc3966999ceab93b25ca8c7c5c400cb36989
Villy-P <valeriuspetrini@gmail.com>
2022-11-22 22:04:21 -0600

    Uncomment

--- src/main.c
+++ src/main.c
@@ -24,31 +24,28 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < numOfComputers; i++) {
             char *iAsString;
             sprintf(iAsString, "%d", i);
-            struct Player computer = newPlayer(strcpy("Computer #", iAsString), 1500, 0, false);
+            struct Player computer = newPlayer(strcpy("Computer #", iAsString), 0, 1500, false);
             struct Node *computerNode = newNode(&computer);
             addToLinkedList(&computers, computerNode);
         }
-        // printf("%sYou go to another table to play some more craps. Place down the money you want to give in return for chips.%s\n", ANSI_ORANGE, ANSI_RESET);
-        // printf("%sYou have $%d%s\n", ANSI_CYAN, mainPlayer.cash, ANSI_RESET);
-        // mainPlayer.chips = getIntInput(">", 1, mainPlayer.cash);
-        // mainPlayer.cash -= mainPlayer.chips;
-        // for (int i = 0; i < numOfComputers; i++) {
-        //     computers[i].chips = computers[i].cash;
-        //     computers[i].cash = 0;
-        // }
-        // player** shooter = &mainPlayer;
-        // printf("%sIt is time for the come out roll%s\n", ANSI_PURPLE, ANSI_RESET);
-        // printf("%sYou pick up two die.%s\n", ANSI_YELLOW, ANSI_RESET);
-        // printf("%sEnter the amount of chips you want to place down. (You have %d chips)%s\n", ANSI_BLUE, shooter.chips, ANSI_RESET);
-        // int chipsToPlace = getIntInput(">", 1, mainPlayer.chips);
-        // printf("%sWhere do you want to place them?%s\n", ANSI_DARK_CYAN, ANSI_RESET);
-        // printf("%s1: Pass%s\n", ANSI_BROWN, ANSI_RESET);
-        // printf("%s2: Don't Pass%s\n", ANSI_GREEN, ANSI_RESET);
-        // int choice = getIntInput(">", 1, 2);
-        // if (choice == 1)
-        //     shooter.chipsOnPass = chipsToPlace;
-        // else
-        //     shooter.chipsOnDontPass = chipsToPlace;
+        printf("%sYou go to another table to play some more craps. Place down the money you want to give in return for chips.%s\n", ANSI_ORANGE, ANSI_RESET);
+        printf("%sYou have $%d%s\n", ANSI_CYAN, mainPlayer.cash, ANSI_RESET);
+        mainPlayer.chips = getIntInput(">", 1, mainPlayer.cash);
+        mainPlayer.cash -= mainPlayer.chips;
+        struct Player* shooter = &mainPlayer;
+        printf("%sIt is time for the come out roll%s\n", ANSI_PURPLE, ANSI_RESET);
+        printf("%sThe stickman passes you five die.%s\n", ANSI_BLUE, ANSI_RESET);
+        printf("%sYou pick up two die.%s\n", ANSI_YELLOW, ANSI_RESET);
+        printf("%sEnter the amount of chips you want to place down. (You have %d chips)%s\n", ANSI_BLUE, shooter->chips, ANSI_RESET);
+        int chipsToPlace = getIntInput(">", 1, mainPlayer.chips);
+        printf("%sWhere do you want to place them?%s\n", ANSI_DARK_CYAN, ANSI_RESET);
+        printf("%s1: Pass%s\n", ANSI_BROWN, ANSI_RESET);
+        printf("%s2: Don't Pass%s\n", ANSI_GREEN, ANSI_RESET);
+        int choice = getIntInput(">", 1, 2);
+        if (choice == 1)
+            shooter->chipsOnPass = chipsToPlace;
+        else
+            shooter->chipsOnDontPass = chipsToPlace;
     }
     return 0;
 }

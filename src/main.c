7d9535c49922174bb13e0522eae8d7c631db0a14
Villy-P <valeriuspetrini@gmail.com>
2022-11-28 21:17:17 -0600

    commit

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "board.h"
 #include "linkedlist.h"
 #include "node.h"
+#include "random.h"
 
 #include <limits.h>
 #include <stdio.h>
@@ -18,13 +19,15 @@ int main(int argc, char *argv[]) {
     printf("%sHow much cash did you bring?%s\n", ANSI_MAGENTA, ANSI_RESET);
     mainPlayer.cash = getIntInput(">", 1, INT_MAX);
     while (true) {
-        printf("%sHow many computers do you want to play against (0-19)%s\n", ANSI_PURPLE, ANSI_RESET);
+        printf("%sHow many computers do you want to play against? (0-19)%s\n", ANSI_PURPLE, ANSI_RESET);
         const int numOfComputers = getIntInput(">", 0, 19);
         struct LinkedList computers = createNewLinkedList();
         for (int i = 0; i < numOfComputers; i++) {
-            char *iAsString;
+            char computerHashtag[] = "Computer #";
+            char iAsString[2];
             sprintf(iAsString, "%d", i);
-            struct Player computer = newPlayer(strcpy("Computer #", iAsString), 0, 1500, false);
+            char *computerName = strcpy(computerHashtag, iAsString);
+            struct Player computer = newPlayer(computerName, 0, 1500, false);
             struct Node *computerNode = newNode(&computer);
             addToLinkedList(&computers, computerNode);
         }
@@ -43,9 +46,33 @@ int main(int argc, char *argv[]) {
         printf("%s2: Don't Pass%s\n", ANSI_GREEN, ANSI_RESET);
         int choice = getIntInput(">", 1, 2);
         if (choice == 1)
-            shooter->chipsOnPass = chipsToPlace;
+            mainPlayer.chipsOnPass = chipsToPlace;
         else
-            shooter->chipsOnDontPass = chipsToPlace;
+            mainPlayer.chipsOnDontPass = chipsToPlace;
+        printf("%sThe computers will now place their bets.%s\n", ANSI_DARK_CYAN, ANSI_RESET);
+        struct Node *currentComputer = computers.head;
+        while (currentComputer != NULL) {
+            int randomValue = randomInRange(0, 10);
+            int chipsToPlaceDown = randomInRange(250, 750);
+            if (randomValue <= 5) { // Bet on pass
+                printf("%s%s placed %d $1 chips on the pass line.%s\n", ANSI_BLUE, currentComputer->player->name, chipsToPlaceDown, ANSI_RESET);
+                currentComputer->player->chips -= chipsToPlaceDown;
+                currentComputer->player->chipsOnPass = chipsToPlaceDown;
+            } else if (randomValue <= 8) { // Bet on dont pass
+                printf("%s%s placed %d $1 chips on the don't pass line.%s\n", ANSI_BLUE, currentComputer->player->name, chipsToPlaceDown, ANSI_RESET);
+                currentComputer->player->chips -= chipsToPlaceDown;
+                currentComputer->player->chipsOnDontPass = chipsToPlaceDown;
+            } else {
+                printf("%s%s did not place any chips down.%s\n", ANSI_BLUE, currentComputer->player->name, ANSI_RESET);
+            }
+            currentComputer = currentComputer->next;
+        }
+        unsigned int firstRoll = randomInRange(1, 6);
+        unsigned int secondRoll = randomInRange(1, 6);
+        printf("%s%s rolls the die...%s\n", ANSI_BLUE, shooter->name, ANSI_RESET);
+        printf("%sThe die hit the back table...%s\n", ANSI_GREEN, ANSI_RESET);
+        printf("%s+---+ +---+%s", ANSI_ORANGE, ANSI_RESET);
+        printf("%s+ %d + + %d +%s");
     }
     return 0;
 }
---
+++ src/random.h
@@ -0,0 +1,6 @@
+#ifndef __C_CRAPS_SRC_RANDOM_H__
+#define __C_CRAPS_SRC_RANDOM_H__
+
+unsigned int randomInRange(unsigned int min, unsigned int max);
+
+#endif
